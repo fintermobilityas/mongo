fd76dd7101203c4df88f85bcc8206fd0522ac3ea
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2024-10-10 07:06:37 +0200

    Update script.

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,8 @@ param (
     [string]$MONGO_VERSION = "8.0.1"
 )
 
-$buildPath = Join-Path -Path (Get-Location) -ChildPath "8.0"
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$buildPath = Join-Path -Path $scriptPath -ChildPath "8.0"
 
 docker build -t "mongo-enterprise:$MONGO_VERSION" $buildPath
 docker tag "mongo-enterprise:$MONGO_VERSION" "ghcr.io/fintermobilityas/mongo-enterprise:$MONGO_VERSION"
