b5113744b023c3c577c7c19c2aad2cbdb7098837
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2025-09-16 08:06:12 +0200

    Update to mongodb 8.2.0.

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,30 @@
 param (
-    [string]$MongoVersion = "8.0.1"
+    [string]$MongoVersion = "8.2.0",
+    [string]$MongoPackageVersion = "8.2.0~latest",
+    [string]$MongoAptChannel = "development",
+    [string]$MongoMongoshChannel = "8.2"
 )
 
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $buildPath = Join-Path -Path $scriptPath -ChildPath "8.0"
 
-docker build -t "mongo-enterprise:$MongoVersion" $buildPath
+$buildArgs = @()
+
+if ($MongoPackageVersion) {
+    $buildArgs += "--build-arg"
+    $buildArgs += "MONGO_VERSION=$MongoPackageVersion"
+}
+
+if ($MongoAptChannel) {
+    $buildArgs += "--build-arg"
+    $buildArgs += "MONGO_MAJOR=$MongoAptChannel"
+}
+
+if ($MongoMongoshChannel) {
+    $buildArgs += "--build-arg"
+    $buildArgs += "MONGOSH_CHANNEL=$MongoMongoshChannel"
+}
+
+docker build @buildArgs -t "mongo-enterprise:$MongoVersion" $buildPath
 docker tag "mongo-enterprise:$MongoVersion" "ghcr.io/fintermobilityas/mongo-enterprise:$MongoVersion"
 docker push "ghcr.io/fintermobilityas/mongo-enterprise:$MongoVersion"
