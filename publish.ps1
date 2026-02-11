199c484174324c9542033d86e4b9b22746bdcefe
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2026-02-11 07:07:50 +0100

    Update to mongodb 8.2.5

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,6 @@
 param (
-    [string]$MongoVersion = "8.2.4",
-    [string]$MongoPackageVersion = "8.2.4~latest",
+    [string]$MongoVersion = "8.2.5",
+    [string]$MongoPackageVersion = "8.2.5",
     [string]$MongoAptChannel = "development",
     [string]$MongoMongoshChannel = "8.2"
 )
