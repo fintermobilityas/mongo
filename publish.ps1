1d0bbfcacef5f1304044969bf1d187b711210467
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2026-08-25 09:01:50 +0200

    Update to mongodb 8.3.10

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,6 @@
 param (
-    [string]$MongoVersion = "8.3.8",
-    [string]$MongoPackageVersion = "8.3.8",
+    [string]$MongoVersion = "8.3.10",
+    [string]$MongoPackageVersion = "8.3.10",
     [string]$MongoAptChannel = "8.3",
     [string]$MongoMongoshChannel = "8.3"
 )
